6d5f8289bbf2addcc8d6693f73f53dc2e808b13e
Andrew <andrew.simmons.dev@gmail.com>
2026-07-14 10:26:29 -0500

    add zoom feature to project blocks

--- my-react-app/src/components/ProjectBlock.jsx
+++ my-react-app/src/components/ProjectBlock.jsx
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import Picture from './Picture';
 import Caption from './Caption';
+import { Controlled as ControlledZoom } from 'react-medium-image-zoom'
 
 function ProjectBlock(props) {
+
+    const [isZoomed, setIsZoomed] = useState(false)
+    
+        const handleZoomChange = useCallback(shouldZoom => {
+            setIsZoomed(shouldZoom)
+        })
+
     return(
-        <div className='block-content'>
-            <Picture imgURL={props.imgURL} />
-            <a href={props.projectURL}><Caption caption={props.caption}/></a>
-        </div>
+        <ControlledZoom isZoomed={isZoomed} onZoomChange={handleZoomChange}>
+            <div className='block-content'>
+                <Picture imgURL={props.imgURL} />
+                <a href={props.projectURL}><Caption caption={props.caption}/></a>
+            </div>
+        </ControlledZoom>
     )
 }
 
